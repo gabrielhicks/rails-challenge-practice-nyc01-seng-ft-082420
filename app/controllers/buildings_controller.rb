ae3bf3a4bd7f7097c8af65c0444c42e49b56ad28
gabrielhicks <gabrielhicks@mail.com>
2020-09-20 14:58:00 -0400

    Add some functionality

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,2 +1,37 @@
 class BuildingsController < ApplicationController
+    before_action :find_building, only: [ :show, :edit, :update, :destroy]
+
+    def index
+        @buildings = Building.all
+    end
+
+    def show
+    end
+
+    def new
+        @building = Building.new
+    end
+
+    def create
+        building = Building.create(building_params)
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def building_params
+        params.require(:building).permit!
+    end
+
+    def find_building
+        @building = Building.find(params[:id])
+    end
 end
--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,2 +1,37 @@
 class CompaniesController < ApplicationController
+    before_action :find_company, only: [ :show, :edit, :update, :destroy]
+    
+    def index
+        @companies = Company.all
+    end
+
+    def show
+    end
+
+    def new
+        @company = Company.new
+    end
+
+    def create
+        company = Company.create(company_params)
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def company_params
+        params.require(:company).permit!
+    end
+
+    def find_company
+        @company = Company.find(params[:id])
+    end
 end
